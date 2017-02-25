bfc9333f8864fe7f86112a223e115a9c87668d19
Bil Benhamou <bil.benhamou@gmail.com>
2017-02-25 23:08:22 +0400

    feat: tried to start coding but got interupted

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hello Namek');
-})
+
+/**
+ * Make
+ * @param {string} userName - Github username
+ */
+
+function getStarredRepos (username) {
+
+    let uri = `https://api.github.com/users/${username}/starred/`;
+
+    // Awesomness to be continues ... 
+
+}
